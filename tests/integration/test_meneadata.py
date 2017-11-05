fe2fd68e62cf14a5e67bc213eb67bfb2ce334bde
Efrain Lima Miranda <elimamiranda@advaoptical.com>
2017-11-05 17:10:53 +0100

    Added first tests

---
+++ tests/integration/test_meneadata.py
@@ -0,0 +1,6 @@
+"""Library integration tests."""
+
+
+def test_example():
+    """Test example."""
+    assert True
---
+++ tests/unit/test_meneadata.py
@@ -0,0 +1,6 @@
+"""Library unit tests."""
+
+
+def test_example():
+    """Test example."""
+    assert True
