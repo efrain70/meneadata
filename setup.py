e7524106d5a6c299c04dd9174ce5c22aa624175f
Efrain Lima Miranda <elimamiranda@advaoptical.com>
2017-11-05 17:10:08 +0100

    Added tox and setup files

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""setup script for building, distributing and installing."""
+
+from setuptools import setup, find_packages
+
+
+with open("README.rst") as readme, open("CHANGELOG.rst") as changelog:
+    LONG_DESCRIPTION = readme.read() + 2 * '\n' + changelog.read()
+
+setup(
+    name='meneadata',
+    description="Meneame Data",
+    long_description=LONG_DESCRIPTION,
+    author="Efraín Lima Miranda",
+    author_email='efrain70@gmail.com',
+    url='https://github.com/efrain70/meneadata',
+    packages=find_packages(),
+    include_package_data=True,
+    use_scm_version={'write_to': 'meneadata/version.py'},
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    install_requires=[
+        'six',
+    ],
+    license="GNU Affero General Public License",
+    zip_safe=False,
+    keywords=['meneadata'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
