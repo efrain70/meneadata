edfb56e304d31a69eef3f3bed06b788d4e01f056
Efrain Lima Miranda <elimamiranda@advaoptical.com>
2017-11-05 18:20:58 +0100

    Added test main command

--- meneadata/meneadata.py
+++ meneadata/meneadata.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-import sys
-
-from .page.main import MainPage
-from .version import version as __version__
-
 
 def main():
-    print("Executing bootstrap version %s." % __version__)
-    print("List of argument strings: %s" % sys.argv[1:])
-    print("MainPage and MainPage():\n%s\n%s" % (MainPage, MainPage()))
+    print('meneadata output')
+    return 0
