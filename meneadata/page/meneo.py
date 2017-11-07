5fbeed634de7899c72c853f8a0e30e199eacd572
Efrain Lima Miranda <elimamiranda@advaoptical.com>
2017-11-07 11:27:13 +0100

    Added author and python2 in meneo data

--- meneadata/page/meneo.py
+++ meneadata/page/meneo.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from collections.abc import Iterable
+from six import Iterator
 
 
-class Meneo(Iterable):
+class Meneo(Iterator):
 
-    headers = ['votes', 'title', ]
+    headers = ['votes', 'title', 'author']
     vote_class = 'votes'
 
     def __init__(self, bs_tag):
@@ -12,13 +12,24 @@ class Meneo(Iterable):
 
     @property
     def title(self):
-        return self.bs_tag.find('h2').text
+        return self.bs_tag.find('h2').text.strip()
 
     @property
     def votes(self):
         votes = self.bs_tag.find('div', class_=self.vote_class)
         return votes.find('a').text
 
+    @property
+    def author(self):
+        author_image = self.bs_tag.find('div', class_='news-submitted')
+        return author_image.find_all('a')[1].text
+
     def __iter__(self):
         yield self.votes
         yield self.title
+        yield self.author
+
+    def __next__(self):
+        return self.__iter__()
+
+    next = __next__  # Python 2
